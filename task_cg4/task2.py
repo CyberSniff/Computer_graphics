fix a lot of bugs, add triangle, add rotation

task_cg4/task2.py
@@ -5,20 +5,20 @@ from tkinter import colorchooser, messagebox
 from PIL import Image, ImageDraw
 import numpy as np
 from random import randint
+import math
 
 from enum import Enum
 
 class ShapeType(Enum):
     point = 0
     segment = 1
-    square = 2
-    circle = 3
+    triangle = 2
+    square = 3
 
-# values - список всех значений, связанных с фигурой
 class Shape:
-    def __init__(self, type, values, color):
+    def __init__(self, type, coordinates, color):
         self.type = type
-        self.values = values
+        self.coordinates = coordinates
         self.color = color
 
 shapes = []
@@ -28,7 +28,7 @@ y = 0
 count_click = 0
 
 root = Tk()
-root.title("Ну почти paint")
+root.title("Задание 4")
 width = 800
 height = 600 
 
@@ -53,7 +53,6 @@ def choose_color():
     color = hx
     color_lab['bg'] = hx
 
-
 def select(value):
     global brush_size
     brush_size= int(value)
@@ -71,29 +70,39 @@ def popup(event):
     x = event.x
     y = event.y
     menu.post(event.x_root, event.y_root)
+
+def triangle():
+    x_1 = x + brush_size
+    y_1 = y
+    x_2 = x + (x_1 - x)/2
+    y_2 =  y - brush_size * 1.73205080757 / 2
+    shapes.append(Shape(ShapeType.triangle, [x, y, x_1, y, x_2, y_2], color))
+
+    canvas.create_polygon(x, y, x_1, y_1, x_2, y_2, fill=color, width=0)
+    draw_img.polygon((x, y, x_1, y_1, x_2, y_2), fill = color)
     
-
 def square():
-    shapes.append(Shape(ShapeType.square, [x, y, brush_size], color))
+    x_1 = x + brush_size
+    y_1 = y
+    x_2 = x_1
+    y_2 = y + brush_size
+    x_3 = x
+    y_3 = y_2
+    shapes.append(Shape(ShapeType.square, [x, y, x_1, y_1, x_2, y_2, x_3, y_3], color))
 
-    canvas.create_rectangle(x, y, x+brush_size, y+brush_size, fill=color, width=0)
-    draw_img.polygon((x, y, x+brush_size, y, x+brush_size, y+brush_size, x, y+brush_size), fill = color)
+    canvas.create_polygon(x, y, x_1, y_1, x_2, y_2, x_3, y_3, fill=color, width=0)
+    draw_img.polygon((x, y, x_1, y_1, x_2, y_2, x_3, y_3), fill = color)
 
 def point():
-    shapes.append(Shape(ShapeType.point, [x, y, 3], color))
+    x_next = x + 3
+    y_next = y + 3
+    shapes.append(Shape(ShapeType.point, [x, y, x_next, y_next], color))
 
-    canvas.create_oval(x, y, x+3, y+3, fill=color, width=0)
-    draw_img.ellipse((x, y, x+3, y+3), fill=color)
-
-def circle():
-    shapes.append(Shape(ShapeType.circle, [x, y, brush_size], color))
-
-    canvas.create_oval(x, y, x+brush_size, y+brush_size, fill=color, width=0)
-    draw_img.ellipse((x, y, x+brush_size, y+brush_size), fill=color)
+    canvas.create_oval(x, y, x_next, y_next, fill=color, width=0)
+    draw_img.ellipse((x, y, x_next, y_next), fill=color)
 
 def segment():
     canvas.bind('<Button-3>', click)
-
 
 def click(event):
     global prev_x, prev_y
@@ -106,7 +115,7 @@ def click(event):
     x = event.x
     y = event.y
 
-    shapes.append(Shape(ShapeType.segment, [prev_x, prev_y, x, y, 3], color))
+    shapes.append(Shape(ShapeType.segment, [prev_x, prev_y, x, y], color))
 
     canvas.create_line(prev_x, prev_y, x, y, width=3, fill=color)
     canvas.bind('<Button-3>', popup)
@@ -120,8 +129,8 @@ canvas.bind('<Button-3>', popup)
 menu = Menu(tearoff=0)
 menu.add_command(label='Точка', command=point)
 menu.add_command(label='Отрезок', command=segment)
+menu.add_command(label='Треугольник', command=triangle)
 menu.add_command(label='Квадрат', command=square)
-menu.add_command(label='Круг', command=circle)
 
 image1 = Image.new('RGB', (width, height), 'white')
 draw_img = ImageDraw.Draw(image1)
@@ -143,48 +152,81 @@ def redraw():
     is_redraw = True
     clear_scene()
     for shape in shapes:
-        x = shape.values[0]
-        y = shape.values[1]
         if shape.type == ShapeType.point:
-            #point()
-            brush_size = shape.values[2]
-            color = shape.color
-            canvas.create_oval(x, y, x + brush_size, y + brush_size, fill=color, width=0)
-            draw_img.ellipse((x, y, x + brush_size, y + brush_size), fill=color)
+            canvas.create_oval(shape.coordinates, fill=shape.color, width=0)
+            draw_img.ellipse(shape.coordinates, fill=shape.color)
         elif shape.type == ShapeType.segment:
-            x_next = shape.values[2]
-            y_next = shape.values[3]
-            color = shape.color
-            canvas.create_line(x, y, x_next, y_next, width=3, fill=color)
-        elif shape.type == ShapeType.square:
-            #square()
-            brush_size = shape.values[2]
-            color = shape.color
-            canvas.create_rectangle(x, y, x + brush_size, y + brush_size, fill=color, width=0)
-            draw_img.polygon((x, y, x + brush_size, y, x + brush_size, y + brush_size, x, y + brush_size), fill = color)
+            canvas.create_line(shape.coordinates, width=3, fill=shape.color)
+        elif shape.type == ShapeType.segment:
+            canvas.create_line(shape.coordinates, width=3, fill=color)
+        elif shape.type == ShapeType.triangle:
+            canvas.create_polygon(shape.coordinates, fill=shape.color, width=0)
+            draw_img.polygon(shape.coordinates, fill = shape.color)
         else:
-            #circle()
-            brush_size = shape.values[2]
-            color = shape.color
-            canvas.create_oval(x, y, x + brush_size, y + brush_size, fill=color, width=0)
-            draw_img.ellipse((x, y, x + brush_size, y + brush_size), fill=color)
+            canvas.create_polygon(shape.coordinates, fill=shape.color, width=0)
+            draw_img.polygon(shape.coordinates, fill = shape.color)
+       
+    var_x_trans.set(0)
+    var_y_trans.set(0)
+    var_rot.set(0)
+    var_x_scal.set(0)
+    var_y_scal.set(0)
 
     is_redraw = False
 
+def coord_mult(shape, matrix):
+    n = len(shape.coordinates)
+    i = 0
+    while i < n:
+        coord = np.array([shape.coordinates[i], shape.coordinates[i + 1], 1])
+        res = coord.dot(matrix)
+        shape.coordinates[i], shape.coordinates[i + 1] = res[0], res[1]
+        i += 2
 
 def translate():
     dx = var_x_trans.get()
     dy = var_y_trans.get()
 
     for shape in shapes:
-        if shape.type == ShapeType.circle or shape.type == ShapeType.square:
-            coord = np.array([shape.values[0], shape.values[1], 1])
+        if shape.type == ShapeType.triangle or shape.type == ShapeType.square:
             matrix = np.array([[1, 0, 0], [0, 1, 0], [dx, -dy, 1]])
-            res = coord.dot(matrix)
-            shape.values[0], shape.values[1] = res[0], res[1]
+            coord_mult(shape, matrix)
     
     redraw()
 
+def find_center(shape):
+    i = 0
+    n = len(shape.coordinates)
+    x_res = 0
+    y_res = 0
+
+    while i < n:
+        x_res += shape.coordinates[i]
+        y_res += shape.coordinates[i + 1]
+        i += 2
+
+    n /= 2
+    x_res /= n
+    y_res /= n
+    return x_res, y_res
+
+def rotate():
+    degree = var_rot.get()
+    sin = math.sin(np.deg2rad(degree))
+    cos = math.cos(np.deg2rad(degree))
+
+    for shape in shapes:
+        if shape.type == ShapeType.triangle or shape.type == ShapeType.square:
+                x_center, y_center = find_center(shape)
+                matrix = np.array([[1, 0, 0], [0, 1, 0], [-x_center, -y_center, 1]])
+                coord_mult(shape, matrix)
+
+                matrix = np.array([[cos, sin, 0], [-sin, cos, 0], [0, 0, 1]])
+                coord_mult(shape, matrix)
+
+                matrix = np.array([[1, 0, 0], [0, 1, 0], [x_center, y_center, 1]])
+                coord_mult(shape, matrix)
+    redraw()
 
 
 Label(root, text="Полигоны", padx=5, pady=5).grid(row=0, column=7)
@@ -197,7 +239,7 @@ var_y_trans = IntVar()
 var_y_trans.set(0)
 scale_y_trans = Scale(root, variable=var_y_trans, from_=-300, to=300, orient=HORIZONTAL, label='Y', length = '150').grid(row=1, column=8)
 
-Button(root, text="Поворот", padx=5, pady=5).grid(row=2, column=6)
+Button(root, text="Поворот", padx=5, pady=5, command=rotate).grid(row=2, column=6)
 var_rot = IntVar()
 var_rot.set(0)
 scale_rot = Scale(root, variable=var_rot, from_=-360, to=360, orient=HORIZONTAL, label='Градусы', length = '150').grid(row=2, column=7)